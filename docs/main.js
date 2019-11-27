4d4bbf22a4640b6552815caabd36145ebd8cd6d4
Vasilije Tomovic <vasilijetomovic98@gmail.com>
2019-11-27 19:57:02 +0100

    We are change keywords and javascrpt file

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,5 @@
-$('.carousel').carousel({
-    interval: 6000,
-    pause: "false"
-});
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    // Your code to run since DOM is loaded and ready
 
+});
